ac532a75a077f3fd7783cf214a2122ac4a8cd2cc
rjp301 <rileypaul96@gmail.com>
2022-02-12 13:31:42 -0700

    Create Python script to generate schema and helper. Update resetdb

---
+++ db/schema/02_maps.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS maps CASCADE;
+CREATE TABLE maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  creator_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  date_created DATE NOT NULL DEFAULT Now(),
+  name varchar(255) NOT NULL,
+  description TEXT NOT NULL
+);
