3418bbb3ce033717e107a9e003cd63bfc828b63f
Jiani Cao <jianicakes@gmail.com>
2022-02-12 16:31:01 -0800

    add render of map-page template

--- routes/maps.js
+++ routes/maps.js
@@ -3,7 +3,8 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/:id", (req, res) => {
-    res.send(`Hello this is maps number ${req.params.id}`);
+    // res.send(`Hello this is maps number ${req.params.id}`);
+    res.render("map-page");
 
     //// IMPLEMENT DATABASE LATER ////
     // db.query(`SELECT * FROM maps WHERE id = $1;`, [req.params.id])
