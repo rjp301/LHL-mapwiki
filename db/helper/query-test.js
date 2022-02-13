a00f1a9d43d938be64e014045c367fb535b6ddce
Jiani Cao <jianicakes@gmail.com>
2022-02-13 09:21:38 -0800

    add demonstration

--- db/helper/query-test.js
+++ db/helper/query-test.js
@@ -7,5 +7,7 @@ const { getMaps,
   updateMap } = require('./map-queries');
 
 const { getUsers } = require('./user-queries');
-
-console.log(getUsers());
+const db = require('./../../lib/db');
+db.query(`SELECT * FROM maps`)
+  .then(res => console.log(res.rows))
+  .catch(err => console.error(err.stack));
