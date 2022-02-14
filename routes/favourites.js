619aa57ee72d1d2740f3590ed075ab596733f571
rjp301 <rileypaul96@gmail.com>
2022-02-14 09:21:36 -0700

    Create favourites routes

---
+++ routes/favourites.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const favouritesQueries = require('./../db/helper/favourite-queries');
+
+router.post('/', (req, res) => {
+  // add editor
+  favouritesQueries
+    .addMapToFavourites(req.body.userId, req.body.mapId)
+    .then(response => res.json(response))
+    .catch(err => console.error(err.stack));
+});
+
+router.post('/:id/delete', (req, res) => {
+  // remove editor
+  favouritesQueries
+    .removeMapFromFavourites(req.body.userId, req.body.mapId)
+    .then(response => res.json(response))
+    .catch(err => console.error(err.stack));
+});
