55e92b2b8d825c1e961ec0c6280cc4190b67302a
rjp301 <rileypaul96@gmail.com>
2022-02-14 16:53:27 -0700

    Bind click handler to heart icon to add maps to favourites

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,18 @@ $().ready(() => {
   $container.append($(`<div id="map-container"></div>`));
 });
 
+const readCookie = (name) => {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(';');
+  for (const i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
+
+
 const bindNavButtons = () => {
   $('#maps-new');
   $('#login-button').click(() => $.get('/login'));
@@ -32,7 +44,7 @@ const renderMaps = (maps) => {
 };
 
 const createMapElement = (map) => {
-  return $(`
+  const $mapCard = $(`
   <div class="card">
     <i class="fa-solid fa-heart heart-icon"></i>
     <img src="https://lh3.googleusercontent.com/hfTxtV7Li6wz2nOM24_imRCnKcEoxZZyI98rZBPlluTAmMpsnb-cg3qSc-OKX4I_FVcwrOHMNHaDceP_GpqB7a178xtaxFDtNu4EgZP6J2kYeboN6u-fIEDdyS7qYdcj4li02eHU59rRVKQT1Pry7NYLCzB0_r2tvLXZv00N9A5fxalYfJK0U-zWsFHj8ZsRTA_WQtkQzAcIF54d_FgZfcL6ANahsppEzJ8rwio3hSVsxA676dc4IIX0AgN5snX5zxUD1oTbYKTOjFXHSmbn30Mi3_CwK1u8eNEAGa_P71N5SFuU_F4XMov5hP4cSTlkiEmPNZuqfjAHf3T74vKVMPdHzL6sdHdMFtvU5brXclTyXPSw54csfHGD-AaCU6Go5LbnWaTobRBJOiQB7d7Mv6svodRm102ONsfbimgqT3MZwdMklUugld4_giz01a24B9hx7_ZieXycWK-6AwpkPhJUWl-vuP_DOXs_YOo0LxFbAq9Nf75dphTWvPQp-dMRgl9BZVJrOI2Ez6uDHGkK3q7LCvnvSMaSkkhO7gR6VGbM8GbJyyicoVYNdoKByX7hYEnsNwPAqgBoCFL_JsvmK-rYgQBiviXYvh_W-tqXL1acjSRvkg2_aaj8Z5OGWi4k-LKFvvEoRzBURM9q_UkJ9Y_a8rcah0lgz6XhIQsMaIDkez352p6nGbOusKpwrzyEKwAeMcEf_KFYy5QDTzQCYq8Q=w920-h485-no?authuser=0" alt="">
@@ -42,5 +54,14 @@ const createMapElement = (map) => {
     </div>
   </div>
   `);
+
+  const $favButton = $mapCard.find('.heart-icon');
+  $favButton.click(() => {
+    $.post('/favourites', {
+      userId: readCookie('userId'),
+      mapId: map.id
+    });
+  });
+  return $mapCard;
 };
 
