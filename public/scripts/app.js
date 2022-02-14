a3a8f8b4374d2f38bf4bbab981382630ab886d7b
rjp301 <rileypaul96@gmail.com>
2022-02-14 15:55:41 -0700

    Change appearance of map cards

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 $().ready(() => {
-  const $container = $('main');
-
   bindNavButtons();
+
+  const $container = $('main');
   $container.append($(`<div id="map-container"></div>`));
 });
 
@@ -33,13 +33,14 @@ const renderMaps = (maps) => {
 
 const createMapElement = (map) => {
   return $(`
-  <div class="card" >
-     <span class="heart-icon"><i class="fa-solid fa-heart"></i></span>
-     <img class="card-img-top" src="https://image.shutterstock.com/image-vector/urban-vector-city-map-johannesburg-600w-1757922890.jpg" alt="Card image cap">
-     <div class="card-body">
-     <h6 class="card-title">${map.name}</h6>
-     <p class="card-text">${map.description}</p>
-     <a href="#" class="btn btn-info  btn-block">Map Page</a>
-  </div>`);
+  <div class="card">
+    <i class="fa-solid fa-heart heart-icon"></i>
+    <img src="https://lh3.googleusercontent.com/hfTxtV7Li6wz2nOM24_imRCnKcEoxZZyI98rZBPlluTAmMpsnb-cg3qSc-OKX4I_FVcwrOHMNHaDceP_GpqB7a178xtaxFDtNu4EgZP6J2kYeboN6u-fIEDdyS7qYdcj4li02eHU59rRVKQT1Pry7NYLCzB0_r2tvLXZv00N9A5fxalYfJK0U-zWsFHj8ZsRTA_WQtkQzAcIF54d_FgZfcL6ANahsppEzJ8rwio3hSVsxA676dc4IIX0AgN5snX5zxUD1oTbYKTOjFXHSmbn30Mi3_CwK1u8eNEAGa_P71N5SFuU_F4XMov5hP4cSTlkiEmPNZuqfjAHf3T74vKVMPdHzL6sdHdMFtvU5brXclTyXPSw54csfHGD-AaCU6Go5LbnWaTobRBJOiQB7d7Mv6svodRm102ONsfbimgqT3MZwdMklUugld4_giz01a24B9hx7_ZieXycWK-6AwpkPhJUWl-vuP_DOXs_YOo0LxFbAq9Nf75dphTWvPQp-dMRgl9BZVJrOI2Ez6uDHGkK3q7LCvnvSMaSkkhO7gR6VGbM8GbJyyicoVYNdoKByX7hYEnsNwPAqgBoCFL_JsvmK-rYgQBiviXYvh_W-tqXL1acjSRvkg2_aaj8Z5OGWi4k-LKFvvEoRzBURM9q_UkJ9Y_a8rcah0lgz6XhIQsMaIDkez352p6nGbOusKpwrzyEKwAeMcEf_KFYy5QDTzQCYq8Q=w920-h485-no?authuser=0" alt="">
+    <div class="card-text">
+      <h3>${map.name}</h3>
+      <p>${map.description}</p>
+    </div>
+  </div>
+  `);
 };
 
